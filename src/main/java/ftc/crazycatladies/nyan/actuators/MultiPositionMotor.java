0e8952202bdc4c590c8a5221df9f720caa600f0d
Jason Gerstenberger <jason.gerstenberger@mckesson.com>
2020-04-19 19:22:06 -0400

    add source & javadoc jar to publication; start javadoc comments in MultiPositionMotor

--- src/main/java/ftc/crazycatladies/nyan/actuators/MultiPositionMotor.java
+++ src/main/java/ftc/crazycatladies/nyan/actuators/MultiPositionMotor.java
@@ -1,12 +1,26 @@
 package ftc.crazycatladies.nyan.actuators;
 
+/**
+ * Moves a motor to set positions defined in the DcMotorPosition enum
+ * @param <P>
+ */
 public class MultiPositionMotor<P extends DcMotorPosition> extends DcMotorEx {
     P currentPosition;
 
+    /**
+     * @param name
+     * @param hubNum
+     * @param isForward
+     */
     public MultiPositionMotor(String name, int hubNum, boolean isForward) {
         super(name, hubNum, isForward);
     }
 
+    /**
+     * @param position
+     * @param timeout
+     * @param power
+     */
     public void moveTo(P position, double timeout, double power) {
         moveTo(position.getPosition(), timeout, power);
         currentPosition = position;
