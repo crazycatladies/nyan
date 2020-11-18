6ddaf619625f9807887424e1148844a159baec21
Jason Gerstenberger <jason@gerstenberger.name>
2020-11-17 19:50:42 -0500

    add BulkDataFetcher

---
+++ src/main/java/ftc/crazycatladies/nyan/subsystem/BulkDataFetcher.java
@@ -0,0 +1,35 @@
+package ftc.crazycatladies.nyan.subsystem;
+
+import com.qualcomm.hardware.lynx.LynxModule;
+import com.qualcomm.hardware.lynx.LynxModuleIntf;
+import com.qualcomm.hardware.lynx.LynxNackException;
+import com.qualcomm.hardware.lynx.commands.core.LynxGetBulkInputDataCommand;
+import com.qualcomm.hardware.lynx.commands.core.LynxGetBulkInputDataResponse;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BulkDataFetcher {
+    private List<LynxModule> expansionHubs;
+
+    public BulkDataFetcher(List<LynxModule> expansionHubs) {
+        this.expansionHubs = expansionHubs;
+    }
+
+    public Map<Integer, LynxGetBulkInputDataResponse> getBulkDataResponse() {
+        HashMap<Integer, LynxGetBulkInputDataResponse> bulkDataResponse = new HashMap<Integer, LynxGetBulkInputDataResponse>();
+        try {
+            for (Object hub : expansionHubs) {
+                LynxGetBulkInputDataCommand bulkCommand = new LynxGetBulkInputDataCommand((LynxModuleIntf) hub);
+                LynxGetBulkInputDataResponse dataResponse = bulkCommand.sendReceive();
+                bulkDataResponse.put(((LynxModuleIntf) hub).getModuleAddress(), dataResponse);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (LynxNackException e) {
+            e.printStackTrace();
+        }
+        return bulkDataResponse;
+    }
+}
